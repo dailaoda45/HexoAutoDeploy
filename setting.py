2be8cb7df5ca730de7d3f1b980e5e16afcdcc588
dailaoda45 <daichangyi940713@foxmail.com>
2024-07-05 01:00:50 +0800

    更新md文件

--- setting.py
+++ setting.py
@@ -1,10 +1,9 @@
 # 主机账号密码
-host = '8.8.8.8'
+host = ''
 user = 'root'
-password = '.'
+password = ''
 # 超时响应时间
 timeout = 10
 
 # 目标路径
 server_path = r'/opt/blog/hexoblog/source/_posts/'
-local_path = r'C:/Users/SS/Desktop/'
