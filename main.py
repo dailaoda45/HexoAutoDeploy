629bd1246fd5fc56e5d2413c36825077f71c974a
dailaoda45 <daichangyi940713@foxmail.com>
2024-07-13 19:12:40 +0800

    第一个完整版本发布！‘

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 import sys
-from PyQt6.QtWidgets import QApplication,QWidget,QFileDialog,QGridLayout,QPushButton,QLabel,QListWidget,QMessageBox,QVBoxLayout
-from mainForm import Ui_MainForm
-from PyQt6 import uic
-import setting
-from PyQt6.QtCore import QTimer,QThread, QWaitCondition, QMutex, QMutexLocker,QObject,pyqtSignal,Qt
+from PyQt6.QtWidgets import QApplication,QWidget,QFileDialog, QLabel, QMessageBox,QVBoxLayout,QMainWindow
+from cryptography.fernet import Fernet
+from MainWindow import Ui_MainWindow
+from settingForm import Ui_seversetting
+from PyQt6.QtCore import QThread, QObject,pyqtSignal,Qt
 from pathlib import Path
 import paramiko
 import os
+import json
+from PyQt6 import uic,QtGui
+import base64
 # 部署线程
 class deployThread(QObject):
     finish_signal=pyqtSignal()
     def __init__(self):
         super(deployThread, self).__init__()
         self.status = True
+        self.config_manager = ConfigManager()  # 实例化 ConfigManager 类
 
     def run(self):
+        config = self.config_manager.load_config()  # 加载配置信息
         result = {'status': 1, 'data': None}  # 返回结果
         # 服务器上脚本
         cmd = '/opt/blog/hexoblog/autodeploy.sh'
         ssh = paramiko.SSHClient()  # 创建一个新的SSHClient实例
-        ssh.banner_timeout = setting.timeout
+        ssh.banner_timeout = int(config['overtime'])
         # 设置host key,如果在"known_hosts"中没有保存相关的信息, SSHClient 默认行为是拒绝连接, 会提示yes/no
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(setting.host, 22, setting.user, setting.password, timeout=setting.timeout)  # 连接远程服务器,超时时间1秒
-        stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout=setting.timeout)  # 执行命令
+        ssh.connect(config["host"], 22, config["user"], config["password"], timeout=int(config['overtime']))  # 连接远程服务器,超时时间1秒
+        stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout=int(config['overtime']))  # 执行命令
         out = stdout.readlines()  # 执行结果,readlines会返回列表
         # 执行状态,0表示成功，1表示失败
         channel = stdout.channel
@@ -43,16 +48,18 @@ class backupThread(QObject):
     def __init__(self):
         super(backupThread, self).__init__()
         self.status = True
+        self.config_manager = ConfigManager()  # 实例化 ConfigManager 类
     def run(self):
+        config = self.config_manager.load_config()  # 加载配置信息
         result = {'status': 1, 'data': None}  # 返回结果
         # 服务器上脚本
         cmd = '/opt/blog/hexoblog/autobackup.sh'
         ssh = paramiko.SSHClient()  # 创建一个新的SSHClient实例
-        ssh.banner_timeout = setting.timeout
+        ssh.banner_timeout = int(config['overtime'])
         # 设置host key,如果在"known_hosts"中没有保存相关的信息, SSHClient 默认行为是拒绝连接, 会提示yes/no
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(setting.host, 22, setting.user, setting.password, timeout=setting.timeout)  # 连接远程服务器,超时时间1秒
-        stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout=setting.timeout)  # 执行命令
+        ssh.connect(config["host"], 22, config["user"], config["password"], timeout=int(config['overtime']))  # 连接远程服务器,超时时间1秒
+        stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout=int(config['overtime']))  # 执行命令
         out = stdout.readlines()  # 执行结果,readlines会返回列表
         # 执行状态,0表示成功，1表示失败
         channel = stdout.channel
@@ -91,12 +98,94 @@ class info(QWidget):
         qr.moveCenter(cp)  # 设置为屏幕的中心，矩形大小不变
         self.move(qr.topLeft())
 
-class window(QWidget):
+class settingForm(QWidget):
     def __init__(self):
         super().__init__()
         # use the Ui_login_form
-        self.ui = Ui_MainForm()
+        self.ui = Ui_seversetting()
         self.ui.setupUi(self)
+        # self.ui = uic.loadUi(r'.\UI\settingForm.ui', self)
+        self.show()
+        self.ui.save.clicked.connect(self.saveset)
+        self.ui.cancel.clicked.connect(self.cancelset)
+        self.ui.overtime.setValidator(QtGui.QIntValidator())
+        self.setvalue()
+
+    def setvalue(self):
+        json_path=os.path.join(os.getenv('APPDATA'), 'Hexomanage','config.json')
+
+        if os.path.exists(json_path):
+            path = os.path.join(os.getenv('APPDATA'), 'Hexomanage', 'secret.key')
+            with open(path, 'rb') as key_file:
+                key = key_file.read()
+            cipher = Fernet(key)
+            with open(json_path, 'r') as config_file:
+                config = json.load(config_file)
+            host = cipher.decrypt(base64.urlsafe_b64decode(config['host'].encode())).decode()
+            user = cipher.decrypt(base64.urlsafe_b64decode(config['user'].encode())).decode()
+            password = cipher.decrypt(base64.urlsafe_b64decode(config['password'].encode())).decode()
+            overtime = cipher.decrypt(base64.urlsafe_b64decode(config['overtime'].encode())).decode()
+            uploadpath = cipher.decrypt(base64.urlsafe_b64decode(config['uploadpath'].encode())).decode()
+            self.ui.host.setText(host)
+            self.ui.user.setText(user)
+            self.ui.password.setText(password)
+            self.ui.overtime.setText(overtime)
+            self.ui.uploadpath.setText(uploadpath)
+
+    def saveset(self):
+        print('saveset')
+        print(self.ui.host.text(),self.ui.user.text(),self.ui.password.text(),self.ui.overtime.text(),self.ui.uploadpath.text())
+    #     检查加密文件以及秘钥是否存在
+        appdata_path=os.getenv('APPDATA')
+        if appdata_path is None:
+            print("无法获取APPDATA环境变量")
+            return
+        key_path = os.path.join(appdata_path, 'Hexomanage', 'secret.key')
+
+        if os.path.exists(key_path):
+            # 加载秘钥
+            print(1)
+            with open(key_path, 'rb') as key_file:
+                key = key_file.read()
+        else:
+            # 创建秘钥
+            print(2)
+            key = Fernet.generate_key()
+            with open(key_path, 'wb') as key_file:
+                key_file.write(key)
+        # 创建加密器
+        cipher = Fernet(key)
+        # 加密信息
+        host = base64.urlsafe_b64encode(cipher.encrypt(self.ui.host.text().encode())).decode()
+        user = base64.urlsafe_b64encode(cipher.encrypt(self.ui.user.text().encode())).decode()
+        password = base64.urlsafe_b64encode(cipher.encrypt(self.ui.password.text().encode())).decode()
+        overtime = base64.urlsafe_b64encode(cipher.encrypt(self.ui.overtime.text().encode())).decode()
+        uploadpath = base64.urlsafe_b64encode(cipher.encrypt(self.ui.uploadpath.text().encode())).decode()
+        # 配置字典
+        config = {
+                     'host':host,
+                     'user':user,
+                 'password':password,
+                 'overtime':overtime,
+            'uploadpath': uploadpath
+        }
+
+        # 将配置保存到JSON文件
+        with open(os.path.join(appdata_path, 'Hexomanage','config.json'), 'w') as config_file:
+            json.dump(config, config_file)
+
+
+    def cancelset(self):
+        self.close()
+        print('cancelset')
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        # use the Ui_login_form
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        # self.ui = uic.loadUi(r'.\UI\MainWindow.ui', self)
         # 设置备份、变量
         self.backup_status = False
         self.deploy_status = False
@@ -112,6 +201,7 @@ class window(QWidget):
         self.ui.getlistButton.clicked.connect(self.getlist)
         self.ui.deleteButton.clicked.connect(self.deletefile)
         self.ui.downloadButton.clicked.connect(self.downloadfile)
+        self.ui.setaction.triggered.connect(self.open_set)
         # 多线程
         # 部署线程
         self.thread = QThread()
@@ -136,10 +226,23 @@ class window(QWidget):
         # 线程开始执行之前，从相关线程发射信号
         self.backupthread.started.connect(self.backup_Thread.run)
 
+        self.config_manager = ConfigManager()  # 实例化 ConfigManager 类
+
+
+
+    def open_set(self):
+
+        # 备份线程
+        self.setthread = QThread()
+        # 实例化线程类
+        self.setForm = settingForm()
+        # moveToThread方法把实例化线程移到Thread管理
+        self.setForm.moveToThread(self.setthread)
 
 
 
     def upload(self):
+        config = self.config_manager.load_config()  # 加载配置信息
         upload_list_path = []
         list_text=''
         # 获取upload_list中条目数
@@ -154,14 +257,15 @@ class window(QWidget):
             answer = QMessageBox.question(self,'确认上传内容',f'确认上传以下文档吗？\n{list_text}',QMessageBox.StandardButton.Yes |QMessageBox.StandardButton.No)
             if answer == QMessageBox.StandardButton.Yes:
                 # 使用sftp上传文件
-                t = paramiko.Transport(setting.host, 22)
-                t.banner_timeout = setting.timeout
-                t.connect(username=setting.user, password=setting.password)
+                t = paramiko.Transport(config['host'], 22)
+                t.banner_timeout = int(config['overtime'])
+                print(config['user'],config['password'])
+                t.connect(username=config['user'], password=config['password'])
                 sftp = paramiko.SFTPClient.from_transport(t)
                 # 这里注意需要是两个完整的带文件名称的路径否则会报错
                 for file_path in upload_list_path:
-                    print(os.path.join(setting.server_path,os.path.basename(file_path)))
-                    sftp.put(file_path,os.path.join(setting.server_path,os.path.basename(file_path)))
+                    print(os.path.join(config['uploadpath'],os.path.basename(file_path)))
+                    sftp.put(file_path,os.path.join(config['uploadpath'],os.path.basename(file_path)))
                 t.close()
                 QMessageBox.information(self, '信息', '上传完成！', QMessageBox.StandardButton.Ok)
             # 获取清单
@@ -246,13 +350,14 @@ class window(QWidget):
 
     def getlist(self):
         print('gitlist')
+        config = self.config_manager.load_config()  # 加载配置信息
         cmd = 'cd /opt/blog/hexoblog/source/_posts && ls -1'
         ssh = paramiko.SSHClient()  # 创建一个新的SSHClient实例
-        ssh.banner_timeout = setting.timeout
+        ssh.banner_timeout = int(config['overtime'])
         # 设置host key,如果在"known_hosts"中没有保存相关的信息, SSHClient 默认行为是拒绝连接, 会提示yes/no
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(setting.host, 22, setting.user, setting.password, timeout=setting.timeout)  # 连接远程服务器,超时时间1秒
-        stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout=setting.timeout)  # 执行命令
+        ssh.connect(config["host"], 22,  config["user"], config["password"], timeout = int(config['overtime']))  # 连接远程服务器,超时时间1秒
+        stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout = int(config['overtime']))  # 执行命令
         out = stdout.readlines()  # 执行结果,readlines会返回列表
         # 执行状态,0表示成功，1表示失败
         channel = stdout.channel
@@ -277,6 +382,7 @@ class window(QWidget):
 
     def deletefile(self):
         # print('deletefile')
+        config = self.config_manager.load_config()  # 加载配置信息
     #     获取文件名使用ssh rm删除同时移除文件
         # 获取当前行
         current_row = self.ui.server_file_list.currentRow()
@@ -287,14 +393,14 @@ class window(QWidget):
                                           QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
             if answer == QMessageBox.StandardButton.Yes:
                 # 拼接命令路径
-                cmd = 'rm -rf '+os.path.join(setting.server_path,serverfilename)
+                cmd = 'rm -rf '+os.path.join(config["uploadpath"],serverfilename)
                 result = {'status': 1, 'data': None}  # 返回结果
                 ssh = paramiko.SSHClient()  # 创建一个新的SSHClient实例
-                ssh.banner_timeout = setting.timeout
+                ssh.banner_timeout = int(config['overtime'])
                 # 设置host key,如果在"known_hosts"中没有保存相关的信息, SSHClient 默认行为是拒绝连接, 会提示yes/no
                 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-                ssh.connect(setting.host, 22, setting.user, setting.password, timeout=setting.timeout)  # 连接远程服务器,超时时间1秒
-                stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout=setting.timeout)  # 执行命令
+                ssh.connect(config["host"], 22, config["user"], config["password"], timeout=int(int(config['overtime'])))  # 连接远程服务器,超时时间1秒
+                stdin, stdout, stderr = ssh.exec_command(cmd, get_pty=True, timeout=int(int(config['overtime'])))  # 执行命令
                 out = stdout.readlines()  # 执行结果,readlines会返回列表
                 # 执行状态,0表示成功，1表示失败
                 channel = stdout.channel
@@ -317,38 +423,84 @@ class window(QWidget):
             QMessageBox.warning(self, '警告！', '请先获取清单！')
 
     def downloadfile(self):
-        print('downloadfile')
+        # print('downloadfile')
+        config = self.config_manager.load_config()  # 加载配置信息
         # 获取当前行
         current_row = self.ui.server_file_list.currentRow()
+        current_serverfilename=''
+        local_path=''
         if current_row >= 0:
             # 获取当前行文本值也就是文件名
-            serverfilename = self.ui.server_file_list.item(current_row).text()
-            print(serverfilename)
+            current_serverfilename = self.ui.server_file_list.item(current_row).text()
+            # print(current_serverfilename)
             dir_name = QFileDialog.getExistingDirectory(self, "选择下载位置")
-            local_path = Path(dir_name)
-            print(local_path)
+            if dir_name:
+                local_path = Path(dir_name)
+                # print(local_path)
+                t = paramiko.Transport(config["host"], 22)
+                t.banner_timeout = int(config['overtime'])
+                t.connect(username=config["user"], password=config["password"])
+                sftp = paramiko.SFTPClient.from_transport(t)
+                # 统一路径为/
+                sftp.get(os.path.join(config["uploadpath"], current_serverfilename),
+                         os.path.join(local_path, current_serverfilename).replace('\\', '/'))
+                t.close()
+                # print('执行完成')
+                QMessageBox.information(self, '信息', '下载完成！')
+            else:
+                QMessageBox.information(self, '信息', '你没有选择任何路径，取消了下载！')
         else:
             self.ui.label.setText('服务器上文件清单：数量为0请先获取清单')
-        t = paramiko.Transport(setting.host,22)
-        t.banner_timeout = setting.timeout
-        t.connect(username=setting.user,password=setting.password)
-        sftp = paramiko.SFTPClient.from_transport(t)
-        # 统一路径为/
-        sftp.get(os.path.join(setting.server_path,serverfilename),os.path.join(local_path,serverfilename).replace('\\','/'))
-        t.close()
-        print('执行完成')
-        QMessageBox.information(self, '信息', '下载完成！')
 
 
-# 生成配置文件
+class ConfigManager():
+    def __init__(self):
+        super().__init__()
+    #     读取配置信息
+    def load_config(self):
+        appdata_path = os.getenv('APPDATA')
+        if appdata_path is None:
+            # QMessageBox.warning(self, '警告！', '无法获取配置路径！')
+            print('无法获取配置路径！')
+            return
 
-# 加密配置文件
+        path = os.path.join(appdata_path, 'Hexomanage', 'secret.key')
 
-# 读取配置文件
+        if not os.path.exists(path):
+            # QMessageBox.warning(self, '警告！', '请先设置服务器配置信息！')
+            print('请先设置服务器配置信息！')
+            return
 
+        with open(path, 'rb') as key_file:
+            key = key_file.read()
 
+        cipher = Fernet(key)
+
+        config_path = os.path.join(appdata_path, 'Hexomanage', 'config.json')
+        if not os.path.exists(config_path):
+            # QMessageBox.warning(self, '警告！', '请先设置服务器配置信息！')
+            print('请先设置服务器配置信息！')
+            return
+
+        with open(config_path, 'r') as config_file:
+            config = json.load(config_file)
+
+        host = cipher.decrypt(base64.urlsafe_b64decode(config['host'].encode())).decode()
+        user = cipher.decrypt(base64.urlsafe_b64decode(config['user'].encode())).decode()
+        password = cipher.decrypt(base64.urlsafe_b64decode(config['password'].encode())).decode()
+        overtime = cipher.decrypt(base64.urlsafe_b64decode(config['overtime'].encode())).decode()
+        uploadpath = cipher.decrypt(base64.urlsafe_b64decode(config['uploadpath'].encode())).decode()
+
+        return  {
+            "host": host,
+            "user": user,
+            "password": password,
+            "overtime": overtime,
+            "uploadpath": uploadpath
+        }
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = window()
+    window = MainWindow()
+
     sys.exit(app.exec())
--- setting.py
+++
@@ -1,9 +0,0 @@
-# 主机账号密码
-host = ''
-user = 'root'
-password = ''
-# 超时响应时间
-timeout = 10
-
-# 目标路径
-server_path = r'/opt/blog/hexoblog/source/_posts/'
